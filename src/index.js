e94e67d8f7e8f7e95074a59d5a2d87953a282504
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-23 10:32:43 -0400

    fir deliverable done

--- src/index.js
+++ src/index.js
@@ -2,3 +2,34 @@ const url = "http://localhost:3000/films"
 const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
+const id = 1
+
+document.addEventListener("DOMContentLoaded", () => {
+    getAMovie();
+})
+
+const getAMovie = () => {
+    fetch(`${url}/${id}`)
+    .then(res => res.json())
+    .then(movie => {renderMovie(movie)})
+}
+
+const renderMovie = (movie) => {
+    const poster = document.getElementById('poster')
+    poster.src = movie.poster
+
+    const title = document.getElementById('title')
+    title.innerText = movie.title
+
+    const runTime = document.getElementById('runtime')
+    runTime.innerText = movie.runtime + " minutes"
+
+    const showTime = document.getElementById('showtime')
+    showTime.innerText = movie.showtime
+
+    const ticketsLeft = document.getElementById('ticket-num')
+    const totalTickets = parseInt(movie.capacity, 10)
+    ticketsLeft.innerText = totalTickets - movie.tickets_sold
+    console.log(ticketsLeft)
+}
+
