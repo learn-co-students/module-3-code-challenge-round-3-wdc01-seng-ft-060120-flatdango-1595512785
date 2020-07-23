e2afd21c4b43d0077dca62fd3046eff832a85616
BartTheScrivner <dframe22@gmail.com>
2020-07-23 11:16:26 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -4,76 +4,80 @@ const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetchMovie(`${url}/1`)
-  fetchMovieList(url)
+  fetchMovie(`${url}/1`);
+  fetchMovieList(url);
 })
 
+// MOVIE LIST 
 function fetchMovieList(url) {
   fetch(url)
   .then(response => response.json())
-  .then(movies => displayMovieList(movies))
+  .then(movies => displayMovieList(movies));
 }
 
 function displayMovieList(movies) {
-  movies.forEach(movie => listMovie(movie))
+  movies.forEach(movie => listMovie(movie));
 }
 
 function listMovie(movie) {
-  const movieList = document.getElementById('films')
-  const filmDiv = document.createElement('div')
-  filmDiv.dataset.id = movie.id
-  filmDiv.classList += 'film'
-  filmDiv.classList += ' item'
+  const movieList = document.getElementById('films');
+  const filmDiv = document.createElement('div');
+  filmDiv.dataset.id = movie.id;
+  filmDiv.classList += 'film';
+  filmDiv.classList += ' item';
   if(movie.tickets_sold === parseInt(movie.capacity)) {
-    filmDiv.classList += ' sold-out'
+    filmDiv.classList += ' sold-out';
   }
-  filmDiv.textContent = movie.title
-  console.log(movieList)
-  movieList.append(filmDiv)
+  filmDiv.textContent = movie.title;
+  movieList.append(filmDiv);
 }
 
+// MOVIE DISPLAY
 function fetchMovie(url) {
   fetch(url)
   .then(response => response.json())
-  .then(movie => displayMovie(movie))
+  .then(movie => displayMovie(movie));
 }
 
 function displayMovie(movie) {
-  const card = document.querySelector('.card')
-  card.dataset.id = movie.id
-  const title = document.getElementById('title')
-  const runtime = document.getElementById('runtime')
-  const filmInfo = document.getElementById('film-info')
-  const showtime = document.getElementById('showtime')
-  const ticketNum = document.getElementById('ticket-num')
-  const poster = document.getElementById('poster')
-  title.innerText = movie.title 
-  runtime.innerText = movie.runtime
-  filmInfo.innerText = movie.description
-  showtime.innerText = movie.showtime
-  ticketNum.dataset.capacity = parseInt(movie.capacity)
-  ticketNum.dataset.sold = movie.tickets_sold
-  ticketNum.innerText = `${parseInt(movie.capacity) - movie.tickets_sold}`
-  poster.src = movie.poster
+  const card = document.querySelector('.card');
+  card.dataset.id = movie.id;
+  const title = document.getElementById('title');
+  const runtime = document.getElementById('runtime');
+  const filmInfo = document.getElementById('film-info');
+  const showtime = document.getElementById('showtime');
+  const ticketNum = document.getElementById('ticket-num');
+  const poster = document.getElementById('poster');
+  title.innerText = movie.title;
+  runtime.innerText = `${movie.runtime} minutes`;
+  filmInfo.innerText = movie.description;
+  showtime.innerText = movie.showtime;
+  ticketNum.dataset.capacity = parseInt(movie.capacity);
+  ticketNum.dataset.sold = movie.tickets_sold;
+  ticketNum.innerText = `${parseInt(movie.capacity) - movie.tickets_sold}`;
+  poster.src = movie.poster;
 }
 
+// EVENT HANDLING
 document.addEventListener('click', (e) => {
   if(e.target.innerText === "Buy Ticket") {
-    const movieToUpdate = e.target.closest('.card')
-    buyTicket(movieToUpdate)
+    const movieToUpdate = e.target.closest('.card');
+    buyTicket(movieToUpdate);
   } else if(e.target.classList.contains('film')) {
-    fetchMovie(`${url}/${e.target.dataset.id}`)
+    fetchMovie(`${url}/${e.target.dataset.id}`);
   }
 });
 
+// BUY A TICKET
 function buyTicket(movieCard) {
-  const ticketData = movieCard.querySelector('#ticket-num')
+  const ticketData = movieCard.querySelector('#ticket-num');
   const sold = ticketData.dataset.sold;
   const capacity = ticketData.dataset.capacity;
+  // Make sure movie isn't sold out
   if(sold < capacity) {
     const movieObj = {
       tickets_sold: parseInt(sold) + 1
-    }
+    };
     const patchRequest = {
       method: 'PATCH',
       headers: {
@@ -81,11 +85,11 @@ function buyTicket(movieCard) {
         Accept: "application/json"
       },
       body: JSON.stringify(movieObj)
-    }
+    };
     fetch(`${url}/${movieCard.dataset.id}`, patchRequest)
     .then(resp => resp.json())
-    .then(movie => displayMovie(movie))
+    .then(movie => displayMovie(movie));
   } else {
-    alert("Sorry, that showing is sold out!")
+    alert("Sorry, that showing is sold out!");
   }
 }
