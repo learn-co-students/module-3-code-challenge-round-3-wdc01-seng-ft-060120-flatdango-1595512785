97af033e95b3502861dabfcb3819dd3344a01e59
zato91 <zatokey@gmail.com>
2020-07-23 11:42:30 -0400

    last commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
 const url1 = "http://localhost:3000/films/1"
+const divCard = showingDiv().firstElementChild
+const divTitle = divCard.getElementsByTagName('div')[0]
+const divRunTime = divCard.getElementsByTagName('div')[1]
+const divDescription = divCard.getElementsByTagName('div')[2].firstElementChild
+const filmInfo = divDescription.getElementsByTagName('div')[0]
+const showtime = document.getElementById("showtime")
+const ticketNum = document.getElementById("ticket-num")
+const ticketString = ticketNum.innerText
+const divExtraContent = divCard.getElementsByTagName('div')[3]
+const buyButton = document.getElementById("orangeButton")
+const ticketing = parseInt(ticketString, 10)
 
 
 const fetchMovie = () => {
@@ -16,23 +27,41 @@ fetchMovie();
 
 function renderMovie(movie){ 
     
-    let divCard = showingDiv().firstElementChild
-    let divTitle = divCard.getElementsByTagName('div')[0]
-    let divRunTime = divCard.getElementsByTagName('div')[1]
-    let divDescription = divCard.getElementsByTagName('div')[2].firstElementChild
-    let filmInfo = divDescription.getElementsByTagName('div')[0]
-    let showtime = document.getElementById("showtime")
-    let ticketNum = document.getElementById("ticket-num")
-    let divExtraContent = divCard.getElementsByTagName('div')[3]
-    console.log(divDescription)
 
     posterDiv().src = movie.poster
     divTitle.innerText = movie.title;   
     divRunTime.innerText = movie.runtime;
     filmInfo.innerText = movie.description;
     showtime.innerText = movie.showtime;
-    ticketNum.innerText = movie.tickets_sold;
+    console.log(ticketing)
+    if (ticketNum.value > 0) {
+        buyButton.style.display = "none";
+    } else {
+        
+        ticketNum.innerText = movie.tickets_sold;
+    }
 }
+buyButton.addEventListener('click', (e) => {
+    
+    const ticketNum = document.getElementById("ticket-num")
+    let ticketvalue =  ticketNum.innerText -= 1
+    
+   
 
+    const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json",
+            },
+             body: JSON.stringify({tickets_sold: ticketvalue })
+    }
+
+    fetch(url1 , options)
+        .then(resp => resp.json())
+        .then(fetchMovie() )  
+ 
+    
+})
 
 })
