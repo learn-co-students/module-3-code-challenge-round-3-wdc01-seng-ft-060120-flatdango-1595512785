c63614ea9ceec421029698e139e79b38bbf42654
mjester93 <m.jester.93@gmail.com>
2020-07-23 10:32:10 -0400

    adding patch for sold ticket

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 const url = "http://localhost:3000/films"
 const filmsDiv = document.querySelector("#films")
-const showingDiv = document.querySelector("#showing")
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const ticketNum = document.querySelector('#ticket-num');
 
     const fetchFirstMovie = () => {
         fetch(url)
         .then(response => response.json())
         .then(movies => renderFirstMovie(movies))
+        .catch(error => alert(error));
+    }
+
+    const sendPatchRequest = (movie) => {
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                tickets_sold: movie.tickets_sold + 1
+            })
+        }
+
+        fetch(`${url}/${movie.id}`, options)
+        .then(response => response.json())
+        .then(movie => updateTickets(movie))
+        .catch(error => alert(error));
     }
 
     const renderFirstMovie = (movies) => {
         const firstMovie = movies[0];
+
+        const showingDiv = document.querySelector("#showing")
+        showingDiv.dataset.movieId = firstMovie.id;
 
         const poster = document.querySelector("#poster")
         poster.src = firstMovie.poster;
@@ -28,8 +51,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const showtime = document.querySelector('#showtime');
         showtime.innerText = firstMovie.showtime;
 
-        const ticketNum = document.querySelector('#ticket-num');
         ticketNum.innerText = parseInt(firstMovie.capacity, 10) - firstMovie.tickets_sold;
+
+        addSoldButtonEventListener(firstMovie);
+    }
+
+    const updateTickets = (movie) => {
+        ticketNum.innerText = parseInt(movie.capacity, 10) - movie.tickets_sold;
+    }
+
+    const addSoldButtonEventListener = (movie) => {
+        const buyButton = document.querySelector(`.button`);
+        buyButton.addEventListener('click', () => {
+            sendPatchRequest(movie);
+
+        })
     }
 
     fetchFirstMovie();
